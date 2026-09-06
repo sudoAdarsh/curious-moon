1659f7bdc95308b1f423496fff7d03471948751e
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2026-09-06 12:06:42 +0530

    Added build.sql

---
+++ code/build.sql
@@ -0,0 +1,20 @@
+drop table if exists master_plan;
+
+create table master_plan(
+  start_time_utc text,
+  duration text,
+  date text,
+  team text,
+  spass_type text,
+  target text,
+  request_name text,
+  library_definition text,
+  title text,
+  description text
+);
+
+COPY master_plan 
+FROM '/home/adarshu/Documents/Coding/curious-moon/cassini-data/master_plan.csv' 
+WITH DELIMITER ',' HEADER CSV;
+
+
